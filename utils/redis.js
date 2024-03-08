113b29dc72e701060815e746d2a612ff808d4501
MarB-T <danmarbet@gmail.com>
2024-03-08 19:36:07 +0300

    setting up redis util

---
+++ utils/redis.js
@@ -0,0 +1,46 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.isConnected = false;
+    this.client = redis.createClient();
+    this.isConnected = true;
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (err) => {
+      this.isConnected = false;
+      console.log(err.message);
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, 'EX', duration);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
