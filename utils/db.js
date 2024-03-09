85b619504da67d4904e8a5c127ad4e1b0d7e858a
MarB-T <danmarbet@gmail.com>
2024-03-09 05:54:58 +0300

    Util file for mongodb

---
+++ utils/db.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${this.host}:${this.port}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.isConnected = false;
+    this.client.connect()
+      .then(() => {
+        this.isConnected = true;
+        this.db = this.client.db(this.database);
+
+        this.client.on('close', () => {
+          this.isConnected = false;
+        });
+
+        this.client.on('reconnect', () => {
+          this.isConnected = true;
+        });
+      })
+      .catch((err) => {
+        console.log('could not connect to MongoDB: ', err);
+      });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    try {
+      return await this.db.collection('users').countDocuments();
+    } catch (error) {
+      console.log(error.message);
+      throw error;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const fileCount = await this.db.collection('files').countDocuments();
+      return fileCount;
+    } catch (error) {
+      console.log(error.message);
+      throw error;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
