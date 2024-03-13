9eca0bed546cd52238faa66d43386c5bf3023ec1
MarB-T <danmarbet@gmail.com>
2024-03-14 00:01:45 +0300

    add user to collection correction

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('users', UserController.postUser);
+router.post('users', UsersController.postUser);
 
 module.exports = router;
